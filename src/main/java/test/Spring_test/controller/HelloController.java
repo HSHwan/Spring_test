7424961cebe4e4c931ea23c06d65f621be56c3cf
HSHwan <tnghks2866@gmail.com>
2024-02-28 23:37:04 +0900

    feat: getmapping method("test-mvc") and test-template.html

--- src/main/java/test/Spring_test/controller/HelloController.java
+++ src/main/java/test/Spring_test/controller/HelloController.java
@@ -3,6 +3,7 @@ package test.Spring_test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "test");
         return "hello";
+    }
+
+    @GetMapping("test-mvc")
+    public String testMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "test-template";
     }
 }
