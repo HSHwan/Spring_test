86ec0e2d31b716dcf08fe59765ba2380a4a98124
HSHwan <tnghks2866@gmail.com>
2024-02-29 22:56:27 +0900

    feat: service and repository implementation

---
+++ src/main/java/test/Spring_test/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package test.Spring_test.repository;
+
+import test.Spring_test.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
