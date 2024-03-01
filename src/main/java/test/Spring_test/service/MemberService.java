4d5892961c930df47d336cbf34ce04b3ee982251
HSHwan <tnghks2866@gmail.com>
2024-03-01 14:32:10 +0900

    feat: service class

---
+++ src/main/java/test/Spring_test/service/MemberService.java
@@ -0,0 +1,40 @@
+package test.Spring_test.service;
+
+import test.Spring_test.domain.Member;
+import test.Spring_test.repository.MemberRepository;
+import test.Spring_test.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * Join Membership
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("You already exist as a member");
+                });
+    }
+
+    /**
+     * Check All Members
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
