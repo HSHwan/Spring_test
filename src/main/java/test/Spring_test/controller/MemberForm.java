2942c5873b16fb6efae2571d708762c446f1a510
HSHwan <tnghks2866@gmail.com>
2024-03-02 22:07:31 +0900

    feat: 회원 가입 및 목록 controller & view 구현

---
+++ src/main/java/test/Spring_test/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package test.Spring_test.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
