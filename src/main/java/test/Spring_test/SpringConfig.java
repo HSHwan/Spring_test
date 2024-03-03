279b86d9298267f52752ec55189d0dd75b89a11a
HSHwan <tnghks2866@gmail.com>
2024-03-03 13:03:05 +0900

    refactor: DB memory -> H2 JDBC 로 변경

--- src/main/java/test/Spring_test/SpringConfig.java
+++ src/main/java/test/Spring_test/SpringConfig.java
@@ -1,13 +1,23 @@
 package test.Spring_test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import test.Spring_test.repository.JdbcMemberRepository;
 import test.Spring_test.repository.MemberRepository;
-import test.Spring_test.repository.MemoryMemberRepository;
 import test.Spring_test.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
