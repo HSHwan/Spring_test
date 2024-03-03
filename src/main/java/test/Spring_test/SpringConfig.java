35df6033bc79735c49b1c256d40f846fe2cbd7de
HSHwan <tnghks2866@gmail.com>
2024-03-03 17:02:54 +0900

    refactor: jdbc -> jdbcTemplate 변경

--- src/main/java/test/Spring_test/SpringConfig.java
+++ src/main/java/test/Spring_test/SpringConfig.java
@@ -3,7 +3,7 @@ package test.Spring_test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import test.Spring_test.repository.JdbcMemberRepository;
+import test.Spring_test.repository.JdbcTemplateMemberRepository;
 import test.Spring_test.repository.MemberRepository;
 import test.Spring_test.service.MemberService;
 
@@ -27,6 +27,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
